b465cb6e6dbf22d523e771ba5e4f429f5384be93
Bikram Chatterjee <chatterjee@bluewin.ch>
2019-09-27 09:24:22 +0200

    forked jamdb debug test oranif only configurable queries maximum possible rows push

--- test/oranif_SUITE.erl
+++ test/oranif_SUITE.erl
@@ -5,7 +5,7 @@
 
 -define(DPI_MAJOR_VERSION, 3).
 -define(DPI_MINOR_VERSION, 0).
--define(MAX_NUMBERS, 100000).
+-define(MAX_VARS, 100000).
 
 all() ->
 	dpi:load_unsafe(),
@@ -23,24 +23,20 @@ test(_) ->
 			]
 		)
 	),
+
 	try
 		setup(Connection, User, Password),
-		{Time, Result} = timer:tc(fun test_i/3, [Connection, User, Password]),
-		if Result /= ok -> ct:pal("ERROR ~p\n", [Result]); true -> ok end,
-		ct:pal(
-			"oranif inserted ~p rows in ~p seconds",
-			[?MAX_NUMBERS, Time / 1000000]
-		)
+		{Time, Inserted} = timer:tc(
+			fun test_i/3, [Connection, User, Password]
+		),
+		Seconds = Time / 1000000,
+		ct:pal("ORANIF inserted ~p rows in ~p seconds", [Inserted, Seconds])
 	catch
 		Class:Error ->
 		ct:pal("ERROR: ~p:~p~n~p", [Class, Error, erlang:get_stacktrace()])
 	end.
 
 test_i(Connection, User, Password) ->
-	%ct:pal(
-	%	"===> Connect with:~n\tConnection ~s~n\tUser ~s~n\tPassword ~s",
-	%	[Connection, User, Password]
-	%),
 	Ctx = dpi:context_create(?DPI_MAJOR_VERSION, ?DPI_MINOR_VERSION),
 	Conn = dpi:conn_create(
 		Ctx, User, Password, Connection,
@@ -48,32 +44,36 @@ test_i(Connection, User, Password) ->
 	),
 
 	#{var := ItemVar} = dpi:conn_newVar(
-	Conn, 'DPI_ORACLE_TYPE_VARCHAR', 'DPI_NATIVE_TYPE_BYTES',
-	?MAX_NUMBERS, 16, true, false, null
+		Conn, 'DPI_ORACLE_TYPE_VARCHAR', 'DPI_NATIVE_TYPE_BYTES',
+		?MAX_VARS, 16, true, false, null
 	),
 
 	Stmt = dpi:conn_prepareStmt(
-	Conn, false, <<"insert into test (ITEM) values (:ITEM)">>, <<>>
+	Conn, false, l2b(ct:get_config(insert_oranif)), <<>>
 	),
 	dpi:stmt_bindByName(Stmt, <<"ITEM">>, ItemVar),
-	set_from_bytes(ItemVar),
-	dpi:stmt_executeMany(Stmt, [], ?MAX_NUMBERS),
+	Inserted = set_from_bytes(ItemVar, Stmt),
 
 	dpi:conn_commit(Conn),
 	dpi:stmt_close(Stmt, <<>>),
 	dpi:var_release(ItemVar),
 	dpi:conn_close(Conn, [], <<>>),
-	dpi:context_destroy(Ctx).
+	dpi:context_destroy(Ctx),
+	Inserted.
 
 l2b(B) when is_binary(B) -> B;
 l2b(L) when is_list(L) -> list_to_binary(L).
 
-set_from_bytes(ItemVar) -> set_from_bytes(0, ItemVar).
-set_from_bytes(Count, _) when Count >= ?MAX_NUMBERS -> ok;
-set_from_bytes(Count, ItemVar) ->
-	Item = list_to_binary(io_lib:format("4179~7..0B", [Count])),
+set_from_bytes(ItemVar, Stmt) -> set_from_bytes(0, 0, ItemVar, Stmt).
+set_from_bytes(Last, Count, ItemVar, Stmt) when Count - Last >= ?MAX_VARS ->
+	case catch dpi:stmt_executeMany(Stmt, [], ?MAX_VARS) of
+		ok -> set_from_bytes(Count, Count, ItemVar, Stmt);
+		_ -> Count
+	end;
+set_from_bytes(LastCount, Count, ItemVar, Stmt) ->
+	Item = list_to_binary(io_lib:format("~10..0B", [Count])),
 	dpi:var_setFromBytes(ItemVar, Count, Item),
-	set_from_bytes(Count + 1, ItemVar).
+	set_from_bytes(LastCount, Count + 1, ItemVar, Stmt).
 
 setup(Connection, User, Password) ->
 	Ctx = dpi:context_create(?DPI_MAJOR_VERSION, ?DPI_MINOR_VERSION),
@@ -82,7 +82,7 @@ setup(Connection, User, Password) ->
 		#{encoding => "AL32UTF8", nencoding => "AL32UTF8"}, #{}
 	),
 
-	DropStmt = dpi:conn_prepareStmt(Conn, false, <<"drop table test">>, <<>>),
+	DropStmt = dpi:conn_prepareStmt(Conn, false, l2b(ct:get_config(drop)), <<>>),
 	case catch dpi:stmt_execute(DropStmt, []) of
 		0 -> ok;
 		{'EXIT', {{error, _, _, #{code := 942}}, _}} -> ok;
@@ -92,7 +92,7 @@ setup(Connection, User, Password) ->
 	dpi:stmt_close(DropStmt, <<>>),
 
 	CreateStmt = dpi:conn_prepareStmt(
-		Conn, false, <<"create table test (item varchar(1000))">>, <<>>
+		Conn, false, l2b(ct:get_config(create)), <<>>
 	),
 	0 = dpi:stmt_execute(CreateStmt, []),
 	dpi:stmt_close(CreateStmt, <<>>),
